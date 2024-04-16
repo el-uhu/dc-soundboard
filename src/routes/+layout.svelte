ada7cc94a79bbaa87c6aa577b9b7dedbb61fd35e
Lukas Hutter <el-uhu@users.noreply.github.com>
2024-04-16 21:22:52 +0200

    set up basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+	import '../app.pcss';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Footer, FooterBrand, FooterCopyright, FooterIcon, FooterLink, FooterLinkGroup } from 'flowbite-svelte';
+</script>
+
+<Navbar  >
+	<NavBrand href="/">
+	  <img src="warbler.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+	  <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Dawn Chorus Soundboard</span>
+	</NavBrand>
+	<NavHamburger  />
+	<NavUl >
+	  <NavLi href="/">Home</NavLi>
+	  <NavLi href="http://el-uhu.github.io/dawnchorus">About</NavLi>
+	</NavUl>
+</Navbar>
+
+<div class="m-5">
+	<slot></slot>
+
+	<div class="p-9">
+		<Footer>
+			<FooterCopyright href="/" by="BG/BRG St. Martin Villach™" year={2024} />
+			<!-- <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+				<FooterLink href="/">About</FooterLink>
+				<FooterLink href="/">Privacy Policy</FooterLink>
+				<FooterLink href="/">Licensing</FooterLink>
+				<FooterLink href="/">Contact</FooterLink>
+			</FooterLinkGroup> -->
+		</Footer>
+	</div>
+
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { Gallery, Button } from 'flowbite-svelte';
+    const images = [
+      { alt: 'eisvogel', src: 'voegel/eisvogel.jpg' },
+      { alt: 'buntspecht', src: 'voegel/buntspecht.jpg' },
+      { alt: 'graureiher', src: 'voegel/graureiher.jpg' },
+      { alt: 'elster', src: 'voegel/elster.jpg' },
+      { alt: 'stockente', src: 'voegel/stockente02.jpg' },
+      { alt: 'storch', src: 'voegel/storch.jpg' },
+      { alt: 'taube', src: 'voegel/taube04.jpg' },
+      { alt: 'uhu', src: 'voegel/uhu.jpg' },
+      { alt: 'neuntoeter', src: 'voegel/neuntoeter.jpg' },
+      { alt: 'rotkehlchen', src: 'voegel/rotkehlchen.jpg' },
+      { alt: 'schwan', src: 'voegel/schwan.jpg' },
+      { alt: 'buntspecht', src: 'voegel/buntspecht.jpg' }
+    ];
+  </script>
+  
+  <div class="flex items-center justify-center py-4 md:py-8 flex-wrap gap-3 mb-3 mx-auto">
+    <Button pill size="xl" outline>Alle</Button>
+    <Button pill size="xl" color="alternative">Singvögel</Button>
+    <Button pill size="xl" color="alternative">Raubvögel</Button>
+    <Button pill size="xl" color="alternative">Wasservögel</Button>
+  </div>
+  
+  <Gallery items={images} class="gap-4 grid-cols-2 md:grid-cols-4" />
