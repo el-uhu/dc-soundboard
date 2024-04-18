a9b4c6118a90b49814d29aaa9087051232763348
Lukas Hutter <el-uhu@users.noreply.github.com>
2024-04-18 18:12:18 +0200

    layout modal

--- src/lib/Gallery.svelte
+++ src/lib/Gallery.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-    import { Modal, Button } from 'flowbite-svelte'
+    import { Modal, Button, Heading, P, A, Secondary, Img } from 'flowbite-svelte'
     import { twMerge } from 'tailwind-merge';
-  
     export let items
     export let imgClass: string = 'h-auto max-w-full rounded-lg';
   
@@ -19,11 +18,26 @@
       <slot {item}>
         <div>
             <button on:click={() => (item.modal = true)}>
-                <img src={item.src} alt={item.alt} class={twMerge(imgClass, $$props.classImg)} />  
+                <img 
+                    src={item.src} 
+                    alt={item.name} 
+                    class={twMerge(imgClass, $$props.classImg)} />  
             </button>
         </div>
-        <Modal title={item.src} bind:open={item.modal} autoclose outsideclose>
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.</p>
+        <Modal title="" bind:open={item.modal} autoclose outsideclose>
+            <Heading tag="h2" customSize="text-4xl font-extrabold ">
+                {item.name}
+                <Secondary class="ms-2">{item.scientific}</Secondary>
+            </Heading>
+
+            <Img src={item.src} size="max-w-md" alignment="mx-auto" alt={item.name} />
+            
+            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                {item.text}
+            </p>
+            <div class="flex items-center justify-center">
+                <iframe src='https://xeno-canto.org/{item.xenoCantoID}/embed' scrolling='no' frameborder='0' width='520px' height='220'></iframe>
+            </div>
           </Modal>
       </slot>
     {:else}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,8 @@
 <script>
     import Gallery from '$lib/Gallery.svelte'
     import { Button } from 'flowbite-svelte';
-    const images = [
-      { alt: 'eisvogel', src: 'voegel/eisvogel.jpg', "modal": false },
-      { alt: 'buntspecht', src: 'voegel/buntspecht.jpg', "modal": false },
-      { alt: 'graureiher', src: 'voegel/graureiher.jpg', "modal": false },
-      { alt: 'elster', src: 'voegel/elster.jpg', "modal": false },
-      { alt: 'stockente', src: 'voegel/stockente02.jpg' , "modal": false },
-      { alt: 'storch', src: 'voegel/storch.jpg' , "modal": false },
-      { alt: 'taube', src: 'voegel/taube04.jpg' , "modal": false },
-      { alt: 'uhu', src: 'voegel/uhu.jpg' , "modal": false },
-      { alt: 'neuntoeter', src: 'voegel/neuntoeter.jpg' , "modal": false },
-      { alt: 'rotkehlchen', src: 'voegel/rotkehlchen.jpg' , "modal": false },
-      { alt: 'schwan', src: 'voegel/schwan.jpg' , "modal": false },
-      { alt: 'buntspecht', src: 'voegel/buntspecht.jpg', "modal": false }
-    ];
-  </script>
-  
-  <div class="flex items-center justify-center py-4 md:py-8 flex-wrap gap-3 mb-3 mx-auto">
-    <Button pill size="xl" outline>Alle</Button>
-    <Button pill size="xl" color="alternative">Singvögel</Button>
-    <Button pill size="xl" color="alternative">Raubvögel</Button>
-    <Button pill size="xl" color="alternative">Wasservögel</Button>
-  </div>
-  
-  <Gallery items={images} class="gap-4 grid-cols-2 md:grid-cols-4" />
+    import images from "$lib/data.json";
+</script>
+
+
+<Gallery items={images} class="gap-4 grid-cols-2 md:grid-cols-6" />
